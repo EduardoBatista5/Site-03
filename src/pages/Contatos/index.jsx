3c293e22c8032e04f9cd2fd47ebd099bc88702ce
EduardoBatista5 <eb215799@gmail.com>
2024-08-03 23:48:28 -0300

    Simplificação no codico das paginas e conteudo adicionado na pagina Contato

--- src/pages/Contatos/index.jsx
+++ src/pages/Contatos/index.jsx
@@ -1,21 +1,37 @@
-import Header from "../../components/Header"
-import Footer from "../../components/Footer"
-import Container from "../../components/Container"
 import styles from './Contatos.module.css' 
+import { IoIosMailOpen, IoLogoInstagram, IoLogoYoutube, IoLogoGithub, IoLogoLinkedin } from "react-icons/io";
 
 function Contatos () {
     return (
         <>
-            <Header />
-            <Container>
+           
             <section className={styles.contatos}>
             <h2>Contatos</h2>
             <h3>Entre em contato</h3>
-            <p>para que possamos conversar mais sobre</p>.
+            <p>para que possamos conversar mais sobre.</p>
+
+            <div className={styles.icones}>
+
+                <a href="https://eb215799@gmail.com" target="_blank" rel="noopener, noreferrer"><IoIosMailOpen className={styles.icone}/>
+                </a>
+                
+                <a href="https://www.instagram.com/eduardo_batistalr/" target="_blank" rel="noopener, noreferrer"><IoLogoInstagram className={styles.icone}/>
+                </a>
+
+                <a href="https://www.youtube.com/channel/UCEOXThnCF9i8nqu2mpVxBVQ" target="_blank" rel="noopener, noreferrer"><IoLogoYoutube className={styles.icone}/>
+                </a>
+
+                <a href="https://github.com/EduardoBatista5" target="_blank" rel="noopener, noreferrer"><IoLogoGithub className={styles.icone}/>
+                </a>
+
+                <a href="Los Eduardos" target="_blank" rel="noopener, noreferrer"><IoLogoLinkedin className={styles.icone}/>
+                </a>
+
+            </div>
+
             </section>
         
-            </Container>
-            <Footer />
+
         </>
     )
 }
